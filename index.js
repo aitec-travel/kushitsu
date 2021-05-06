a2b1f37b0ed338b0a93075ec0c2ccd96711e0fc1
aitec-travel <82422182+aitec-travel@users.noreply.github.com>
2021-05-06 17:19:46 +0900

    Update index.js

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ $(function () {
         var msg = `出発希望日：${date}\n人数：${number}\n氏名：${names}`;
         var msg2 = `希望教習所：${selectedValue}`;
         sendText(msg);
+        sleep(300);
         sendText(msg2);
         
         return false;
